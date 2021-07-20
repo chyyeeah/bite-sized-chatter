6a13af976b9125ee54fc3d02d18d6420d00e742f
chyyeeah <wilsoncheah5@gmail.com>
2021-07-19 17:45:32 -0700

    cleaned up socket event: offer

--- client/dist/watch/watch.js
+++ client/dist/watch/watch.js
@@ -1,8 +1,15 @@
 (async () => {
-  // const splitPathname = window.location.pathname.split('/');
-  // const room = splitPathname[splitPathname.length - 2];
   const searchParams = new URLSearchParams(window.location.search);
   const room = searchParams.get('room');
+
+  const { RTCPeerConnection, RTCSessionDescription } = window;
+  const peerConnection = new RTCPeerConnection();
+  peerConnection.ontrack = (event) => {
+    console.log(event);
+    const remoteVideo = document.getElementById('remote-video');
+    remoteVideo.srcObject = event.streams[0];
+    console.log('srcObject', remoteVideo.srcObject);
+  };
 
   try {
     const response = await fetch('http://localhost:3000/validateroom', {
@@ -15,18 +22,19 @@
 
     if (response.status === 200) {
       socket = io();
-      socket.emit('join', { room });
-      const videoNode = document.createElement('video');
-      document.getElementById('viewing-area').appendChild(videoNode);
-      socket.on('view', async ({ offer }) => {
-        console.log('hit');
-        const { RTCPeerConnection, RTCSessionDescription } = window;
-        const peerConnection = new RTCPeerConnection();
-        await peerConnection.setLocalDescription(new RTCSessionDescription(offer));
-        const answer = await peerConnection.createAnswer();
-        await peerConnection.setLocalDescription(new RTCSessionDescription(answer));
+      // request
+      socket.emit('request', { room });
+      socket.on('offer', async ({ host, offer }) => {
+        try {
+          await peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
+          const answer = await peerConnection.createAnswer();
+          await peerConnection.setLocalDescription(new RTCSessionDescription(answer));
+          socket.emit('answer', { host, answer });
+          console.log('answer emitted');
+        } catch (e) {
+          console.error(e);
+        }
       });
-      socket.emit('watch', { room });
     }
   } catch (e) {
     console.error(e);
