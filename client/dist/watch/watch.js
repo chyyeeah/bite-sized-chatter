905874ed2fc96d9066aea81d9f8001cfab08e09a
chyyeeah <wilsoncheah5@gmail.com>
2021-07-20 20:35:10 -0700

    - refactored code for modularity

--- client/dist/watch/watch.js
+++ client/dist/watch/watch.js
@@ -1,42 +1,44 @@
-(async () => {
-  const searchParams = new URLSearchParams(window.location.search);
-  const room = searchParams.get('room');
+const searchParams = new URLSearchParams(window.location.search),
+  room = searchParams.get('room'),
+  remoteVideo = document.getElementById('remote-video'),
+  { RTCPeerConnection, RTCSessionDescription } = window,
+  config = { 'iceServers': [{ 'urls': 'stun:stun.l.google.com:19302' }] },
+  socket = io(),
+  peerConnection = new RTCPeerConnection();
 
-  const { RTCPeerConnection, RTCSessionDescription } = window;
-  const peerConnection = new RTCPeerConnection();
-  peerConnection.ontrack = (event) => {
-    console.log(event);
-    const remoteVideo = document.getElementById('remote-video');
-    remoteVideo.srcObject = event.streams[0];
-    console.log('srcObject', remoteVideo.srcObject);
-  };
+peerConnection.ontrack = (event) => remoteVideo.srcObject = event.streams[0];
 
+socket.on('offer', async ({ host, offer }) => {
   try {
-    const response = await fetch('http://localhost:3000/validateroom', {
-      method: 'POST',
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ room })
-    });
-
-    let socket;
-
-    if (response.status === 200) {
-      socket = io();
-      // request
-      socket.emit('request', { room });
-      socket.on('offer', async ({ host, offer }) => {
-        try {
-          await peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
-          const answer = await peerConnection.createAnswer();
-          await peerConnection.setLocalDescription(new RTCSessionDescription(answer));
-          socket.emit('answer', { host, answer });
-          console.log('answer emitted');
-        } catch (e) {
-          console.error(e);
-        }
-      });
-    }
+    await peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
+    // console.log('received offer', peerConnection.connectionState);
+    const answer = await peerConnection.createAnswer();
+    await peerConnection.setLocalDescription(new RTCSessionDescription(answer));
+    socket.emit('answer', { host, answer });
+    // console.log('emitted answer', peerConnection.connectionState);
   } catch (e) {
     console.error(e);
   }
-})();
+});
+
+const validateRoom = (room) => {
+  return fetch('http://localhost:3000/validateroom', {
+    method: 'POST',
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ room })
+  });
+};
+
+const joinBroadcast = async (room) => {
+  try {
+    const response = await validateRoom(room);
+    if (response.status === 200) {
+      socket.emit('request', { room });
+      // console.log('emitted request');
+    }
+  } catch (e) {
+    console.error('Error validating room', e);
+  }
+};
+
+joinBroadcast(room);
