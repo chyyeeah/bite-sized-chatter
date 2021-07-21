78cce064e372896f50dc7c03bd8db23b3fff92f6
chyyeeah <wilsoncheah5@gmail.com>
2021-07-21 13:09:21 -0700

    - added a connections hash map - created a new peer connection for each viewer - added a addLocalStreamToPeerConnection function

--- client/dist/broadcast/broadcast.js
+++ client/dist/broadcast/broadcast.js
@@ -3,19 +3,26 @@ const startBroadcast = document.getElementById('start-broadcast'),
   config = { 'iceServers': [{ 'urls': 'stun:stun.l.google.com:19302' }] },
   { RTCPeerConnection, RTCSessionDescription } = window,
   socket = io(),
-  peerConnection = new RTCPeerConnection();
+  connections = {};
+let localStream;
 
 socket.on('request', async ({ viewer }) => {
+  const pc = new RTCPeerConnection();
+  connections[viewer] = pc;
+  addLocalStreamToPeerConnection(pc);
   callUser(viewer);
 });
 
 socket.on('answer', async ({ viewer, answer }) => {
+  const peerConnection = connections[viewer];
   await peerConnection.setRemoteDescription(new RTCSessionDescription(answer));
   // console.log('on answer', peerConnection.connectionState, peerConnection);
+  // console.log('viewer:', viewer);
   callUser(viewer);
 });
 
 const callUser = async (viewer) => {
+  const peerConnection = connections[viewer];
   // console.log('on request', peerConnection.connectionState);
   const offer = await peerConnection.createOffer();
   await peerConnection.setLocalDescription(new RTCSessionDescription(offer));
@@ -24,10 +31,12 @@ const callUser = async (viewer) => {
 };
 
 const setUpLocalStream = async () => {
-  const localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+  localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
   const localVideo = document.getElementById('local-video');
   localVideo.srcObject = localStream;
+};
 
+const addLocalStreamToPeerConnection = (peerConnection) => {
   localStream.getTracks().forEach((track) => peerConnection.addTrack(track, localStream));
 };
 
